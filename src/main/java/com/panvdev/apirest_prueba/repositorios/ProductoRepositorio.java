7ed3e568c3189acca52984d3740a9fb280d6b80a
JuliMart <juli.martinezm@duocuc.cl>
2024-06-16 14:39:12 -0400

    campo comuna

--- src/main/java/com/panvdev/apirest_prueba/repositorios/ProductoRepositorio.java
+++ src/main/java/com/panvdev/apirest_prueba/repositorios/ProductoRepositorio.java
@@ -1,10 +1,10 @@
 package com.panvdev.apirest_prueba.repositorios;
 
-import com.panvdev.apirest_prueba.modelos.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.panvdev.apirest_prueba.modelos.Cliente;
+import com.panvdev.apirest_prueba.modelos.Producto;
+
 
 @Repository
 public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
